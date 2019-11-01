9d8d940d669c505d0ee154d515fbdc0b15224d7f
Nero Li <Nero.Li@informa.com>
2019-11-01 09:45:33 +0800

    update

---
+++ precache-manifest.cfddca22f72dc95ac657f3ecef1d64a9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910eeae76ec239636cb4cafe8202cee5",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "fc52709b6527faf20e77",
+    "url": "/task/static/css/2.0194576d.chunk.css"
+  },
+  {
+    "revision": "279351679a739e50d872",
+    "url": "/task/static/css/main.6da488fc.chunk.css"
+  },
+  {
+    "revision": "fc52709b6527faf20e77",
+    "url": "/task/static/js/2.05f0c144.chunk.js"
+  },
+  {
+    "revision": "279351679a739e50d872",
+    "url": "/task/static/js/main.63f82853.chunk.js"
+  },
+  {
+    "revision": "f715dc4af397f643c076",
+    "url": "/task/static/js/runtime~main.f79bd0da.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/task/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/task/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/task/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/task/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "6f373829a723a34600dd8a22c505e50a",
+    "url": "/task/static/media/star_emtpy.6f373829.png"
+  },
+  {
+    "revision": "0178cc98a1426411d08d121ef52c84cf",
+    "url": "/task/static/media/star_fill.0178cc98.png"
+  }
+]);
